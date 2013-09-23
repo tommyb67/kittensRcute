a8f645b93063f20d3ba2e8bb67edb3ddc5ee606b
Tom Brennan <tommybrennan@gmail.com.com>
2013-09-23 15:30:55 -0400

    changes to main.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 puts 'Kittens are cute'
 puts 'Dogs are cute too'
 =======
 >>>>>>> parent of 0ef8a62... change to main.rb file
+=======
+puts 'Kittens are cute'
+>>>>>>> parent of 22e196c... dogs are cute
