ff1c683cd8501a1309c0e6956eb7929dd48c93f9
Tom Brennan <tommybrennan@gmail.com.com>
2013-09-23 15:36:21 -0400

    removed all the noise in the file

--- main.rb
+++ main.rb
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 puts 'Kittens are cute'
 puts 'Dogs are cute too'
-=======
->>>>>>> parent of 0ef8a62... change to main.rb file
-=======
-puts 'Kittens are cute'
->>>>>>> parent of 22e196c... dogs are cute
-=======
-puts 'Kittens are cute'
->>>>>>> parent of 22e196c... dogs are cute
+
+
+
